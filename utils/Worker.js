6aebada18085d324636ef191ad5955434fd5d8f0
Nishan46 <96242982+Nishan46@users.noreply.github.com>
2023-02-12 16:53:50 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ utils/Worker.js
@@ -0,0 +1,14 @@
+import Heavy from './workers/heavy';
+
+addEventListener('message', (event) =>{
+    console.log(event.data);
+})
+
+
+setInterval(() => {
+    postMessage("go on")
+}, 10000);
+
+
+
+// 1800000
